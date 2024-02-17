21a9f226fe25fbb32747f52018289171e7827ae2
mdnoman58 <mdnoman51696@gmail.com>
2024-02-17 18:48:36 +0600

    first commit

---
+++ dart new project.dart
@@ -0,0 +1,24 @@
+main(){
+  //integer=>1,2,3,343656,=> int
+  // floating=>3.45454,5.34435,2.467 => double
+  //string/textual=>a,b,c,Utso,rafat,noman=>string
+  //boolean=>true/false=>bool
+
+  // container
+  //variable creation syntax
+  //data-type variable-name=value
+
+  //camelcase
+  int firstNumber=20;
+  int secondNumber=30;
+  int additionResult=firstNumber+secondNumber;
+  print(additionResult);
+  print("hello noman");
+
+  //double
+  double cGpa=3.34;
+  print(cGpa);
+  //string
+  String myName= 'Noman';
+  print(myName);
+}
---
+++ dart.dart
@@ -0,0 +1,15 @@
+void main(){
+  String age='12';
+  String index='12123';
+  String result;
+  String name='this is noman';
+  result=age+index;//this is how we get the length of string
+  print(age.length);
+  print(index.length);//this is how we get the any value of inside the string
+
+
+
+
+}
+
+
